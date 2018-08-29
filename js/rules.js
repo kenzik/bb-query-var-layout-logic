92576fec3db5478b0ad1330dde38ff01e1276357
David Kenzik <david@kenzik.com>
2018-08-29 21:38:50 +0000

    js formatting cleanup

--- js/rules.js
+++ js/rules.js
@@ -2,19 +2,20 @@ var addRuleType = BBLogic.api.addRuleType
 var __ = BBLogic.i18n.__
 
 addRuleType( 'bb-query-var-layout-logic/layout-query-var', {
-  label: __( 'Predefined Query Var' ),
-  category: 'general',
-  form: {
-    operator: {
-    type: 'operator',
-    operators: [
-      'equals',
-      'does_not_equal'    ],
-  },
-
-  var: {
-    type: 'string',
-    defaultValue: ''
-  },
-},
-} );
+    label: __( 'Predefined Query Var' ),
+    category: 'general',
+    form: {
+      operator: {
+        type: 'operator',
+        operators: [
+          'equals',
+          'does_not_equal'
+        ],
+      },
+      var: {
+        type: 'string',
+        defaultValue: '',
+      }
+    }
+  }
+);
